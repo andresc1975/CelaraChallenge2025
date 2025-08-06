79375a7c884c690c1c25f1245a3b88b315e57458
andresc1975 <111806674+andresc1975@users.noreply.github.com>
2025-08-05 23:55:59 -0300

    Solucion del challenge
    Incluyo archivos de ejemplo usados para testear y alguna consideraciones.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// ***
+// CeleraChallenge - Test Example
+// ***
+using CelaraChallenge;
+
+
+string path = "c:/temp";
+string matrixFileName = Path.Combine(path, "charMatrix.txt");
+string wordStreamFileName = Path.Combine(path, "wordStream.txt");
+
+
+var matrix = File.ReadLines(matrixFileName);
+var wordStream = File.ReadLines(wordStreamFileName);
+
+var wordFinder = new WordFinder(matrix);
+var result = wordFinder.Find(wordStream);
+
+if (result.Count() > 0)
+{
+    Console.WriteLine("The most repeted words of the stream are: ");
+    foreach (var word in result)
+        Console.WriteLine(word);
+}
+else
+{
+    Console.WriteLine("No words found on the matrix");
+}
---
+++ WordFinder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace CelaraChallenge
+{
+    public class WordFinder
+    {
+        private string[] rows;
+        private string[] columns;
+        
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            int size = matrix.Count();
+            columns = new string[size];
+            rows = matrix.ToArray();
+
+            // To optimize words search, I duplicate information by saving the columns as an array of Strings
+            for (int c = 0; c < size; c++)
+            {
+                string element = "";
+                for (int r = 0; r < size; r++)
+                {
+                    element += rows[r][c];
+                }
+                columns[c] = element;
+            }
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            Dictionary<string, int> searchSummary = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            Dictionary<string, int> foundSummary = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (string word in wordstream)
+            {
+                if (!searchSummary.ContainsKey(word))
+                {
+                    int countInRows = 0;
+                    int countInColumns = 0;
+                    foreach (string row in rows)
+                    {
+                        countInRows += CountOccurrences(word, row);
+                    }
+                    foreach (string col in columns)
+                    {
+                        countInColumns += CountOccurrences(word,col);
+                    }
+                    int total = countInRows + countInColumns;
+                    searchSummary.Add(word, total);
+
+                    if (total > 0)
+                    {
+                        foundSummary.Add(word, total);
+                    }
+                }
+
+            }
+
+            // If the number of words found could be very large,
+            // to get the 10 most repeated ones, I would use some kind of queue or cache,
+            // but given the 64x64 matrix limit, I think that's not necessary.
+            var topTenMostRepeated = foundSummary
+                .OrderByDescending(kv => kv.Value)
+                .Take(10)
+                .Select(kv => kv.Key);
+
+            return topTenMostRepeated;
+        }
+
+
+        private int CountOccurrences(string word, string line)
+        {
+            int count = 0;
+            int index = 0;
+            while ((index = line.IndexOf(word, index, StringComparison.OrdinalIgnoreCase)) >= 0)
+            {
+                count++;
+                index += word.Length;
+            }
+            return count;
+        }
+
+    }
+
+}
